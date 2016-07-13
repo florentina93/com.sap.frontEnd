294de1b87ca9c6f236d0feb6da8bb102edadfae1
florentina93 <and.dragu@gmail.com>
2016-07-13 15:32:11 +0300

    server.xml

---
+++ src/com/sap/frontEnd/servlet/FirstServlet.java
@@ -0,0 +1,16 @@
+package com.sap.frontEnd.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+	
+@Override
+protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	resp.getWriter().println("Hello from frontEnd");
+}
+}
