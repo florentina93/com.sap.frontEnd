9ab6d57c10e9c1c5ef0a9f4249df7ead87b7a986
florentina93 <and.dragu@gmail.com>
2016-08-22 10:54:24 +0300

    validation search

--- src/main/webapp/view/FacebookMaster.controller.js
+++ src/main/webapp/view/FacebookMaster.controller.js
@@ -27,33 +27,36 @@ sap.ui.controller("sap.ui.demo.myFiori.view.FacebookMaster", {
 		},
 		
 		handleSearch : function(evt){
-			this.getView().byId("list").setBusy(true);
-			var oModel = new sap.ui.model.json.JSONModel();
-			var pathModel = "http://localhost:8080/com.sap.crawler/webapi/facebook/posts";
-			var facebookController=this;
-			oModel.attachRequestCompleted(function() {
-				facebookController.getView().byId("list").setBusy(false);
-				console.log(oModel.getData());
-		    });
-			oModel.loadData(pathModel);		
-			this.getView().setModel(oModel);
-			
-			var aFilters = [];
 			var query = evt.getParameter("query");
-			console.log(query);
-			
-			var descriptionButton = this.getView().byId("radioDescription").getSelected();
-			var nameButton = this.getView().byId("radioName").getSelected();
-			var messageButton = this.getView().byId("radioMessage").getSelected();
-			var linkButton = this.getView().byId("radioLink").getSelected();
-			console.log(descriptionButton);
-			
-			var filterName;
-			var filterDesc;
-			var filterLink;
-			var filterMessage;
-			
+			console.log("Query is: " + query);
+
 			if(query && query.length > 0) {
+				
+				this.getView().byId("list").setBusy(true);
+				var oModel = new sap.ui.model.json.JSONModel();
+				var pathModel = "http://localhost:8080/com.sap.crawler/webapi/facebook/posts";
+				var facebookController=this;
+				oModel.attachRequestCompleted(function() {
+					facebookController.getView().byId("list").setBusy(false);
+					console.log(oModel.getData());
+			    });
+				oModel.loadData(pathModel);		
+				this.getView().setModel(oModel);
+				
+				var aFilters = [];
+				
+				var descriptionButton = this.getView().byId("radioDescription").getSelected();
+				var nameButton = this.getView().byId("radioName").getSelected();
+				var messageButton = this.getView().byId("radioMessage").getSelected();
+				var linkButton = this.getView().byId("radioLink").getSelected();
+				console.log(descriptionButton);
+				
+				var filterName;
+				var filterDesc;
+				var filterLink;
+				var filterMessage;
+				
+				 
 				if(descriptionButton == true) {
 					filterDesc = new sap.ui.model.Filter("description", sap.ui.model.FilterOperator.Contains, query);
 					console.log("Description selected, search by description!");
@@ -71,10 +74,11 @@ sap.ui.controller("sap.ui.demo.myFiori.view.FacebookMaster", {
 					filterLink = new sap.ui.model.Filter("linkId", sap.ui.model.FilterOperator.Contains, query);
 					aFilters.push(filterLink);
 				}
+				
+				
+				var list = this.getView().byId("list");
+				var binding = list.getBinding("items");
+				binding.filter(aFilters);
 			}
-			
-			var list = this.getView().byId("list");
-			var binding = list.getBinding("items");
-			binding.filter(aFilters);
 		},
 });
--- src/main/webapp/view/StackOvMaster.controller.js
+++ src/main/webapp/view/StackOvMaster.controller.js
@@ -27,42 +27,43 @@ sap.ui.controller("sap.ui.demo.myFiori.view.StackOvMaster", {
 		},
 	
 		handleSearch : function(evt){
-			this.getView().byId("list").setBusy(true);
-			var oModel = new sap.ui.model.json.JSONModel();
-			
-			var tagButton = this.getView().byId("radioTag").getSelected();
-			var pathModel;
-			
 			var query = evt.getParameter("query");
-			console.log(query);
-			this._oQuery=query;
-			if(tagButton==false) this._oQuery="allTags";
-			pathModel="http://localhost:8080/com.sap.crawler/webapi/stackov/questions";
-			if((tagButton==true) && (query && query.length > 0)){
-				pathModel = "http://localhost:8080/com.sap.crawler/webapi/stackov/questions?tag="+query;
-			}
-			
-			var stackOvController=this;
-			oModel.attachRequestCompleted(function() {
-				stackOvController.getView().byId("list").setBusy(false);
-				console.log(oModel.getData());
-		    });
-			oModel.loadData(pathModel);	
-			console.log(pathModel);
-			this.getView().setModel(oModel);
-			
-			var aFilters = [];
-			
-			var titleButton = this.getView().byId("radioTitle").getSelected();
-			var ownerNameButton = this.getView().byId("radioOwnerName").getSelected();
-			var creationDateButton = this.getView().byId("radioCreationDate").getSelected();
-			
+			console.log("Query is: " + query);
 			if(query && query.length > 0) {
+			
+				this.getView().byId("list").setBusy(true);
+				var oModel = new sap.ui.model.json.JSONModel();
+				
+				var tagButton = this.getView().byId("radioTag").getSelected();
+				var pathModel;
+				
+				this._oQuery=query;
+				if(tagButton==false) this._oQuery="allTags";
+				pathModel="http://localhost:8080/com.sap.crawler/webapi/stackov/questions";
+				if(tagButton==true) {
+					pathModel = "http://localhost:8080/com.sap.crawler/webapi/stackov/questions?tag="+query;
+				}
+				
+				var stackOvController=this;
+				oModel.attachRequestCompleted(function() {
+					stackOvController.getView().byId("list").setBusy(false);
+					console.log(oModel.getData());
+			    });
+				oModel.loadData(pathModel);	
+				console.log(pathModel);
+				this.getView().setModel(oModel);
+				
+				var aFilters = [];
+				
+				var titleButton = this.getView().byId("radioTitle").getSelected();
+				var ownerNameButton = this.getView().byId("radioOwnerName").getSelected();
+				var creationDateButton = this.getView().byId("radioCreationDate").getSelected();
+				
 				if(titleButton == true) {
 					var filterTitle = new sap.ui.model.Filter("title", sap.ui.model.FilterOperator.Contains, query);
 					aFilters.push(filterTitle);
 				} 
-
+	
 				if(creationDateButton == true) {
 					var filterCr = new sap.ui.model.Filter("creationDate", sap.ui.model.FilterOperator.Contains, query);
 					aFilters.push(filterCr);
@@ -70,12 +71,12 @@ sap.ui.controller("sap.ui.demo.myFiori.view.StackOvMaster", {
 				if(ownerNameButton == true) {
 					var filterOwner = new sap.ui.model.Filter("ownerName", sap.ui.model.FilterOperator.Contains, query);
 					aFilters.push(filterOwner);
-				}
+				}			
+				
+				var list = this.getView().byId("list");
+				var binding = list.getBinding("items");
+				binding.filter(aFilters); 
 			}
-			
-			var list = this.getView().byId("list");
-			var binding = list.getBinding("items");
-			binding.filter(aFilters);
 		},
 
 });
