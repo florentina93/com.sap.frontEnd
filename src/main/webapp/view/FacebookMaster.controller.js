1774d4ac4a84fc124d93d2e301184ab003a2dbb3
Patricia008 <petrut.patricia@gmail.com>
2016-08-24 15:35:39 +0300

    not working get binding context, added listitems from controller

--- src/main/webapp/view/FacebookMaster.controller.js
+++ src/main/webapp/view/FacebookMaster.controller.js
@@ -5,25 +5,48 @@ sap.ui.controller("sap.ui.demo.myFiori.view.FacebookMaster", {
 		},
 	
 		onInit:function(){
-			
 			console.log("in master "+this.getRouter());
-			
+			this.getRouter().attachRouteMatched(this.cleanPage , this);
 		    if (sap.ui.Device.system.phone) {
 				return;
-		    }	   
+		    }	  
+		},
+		
+		cleanPage : function()  {  
+			
+		},
+		
+		onBeforeRendering:function(){
+			console.log("before RENERING");
 		},
 		
 		handleNavButtonPress : function(evt) {
 			this.getRouter().navTo("InitialDetail");
-			this.getRouter().navTo("InitialMaster");  
+			this.getRouter().navTo("InitialMaster");
+			var oModel = new sap.ui.model.json.JSONModel();
+			this.getView().setModel(oModel);
 			console.log("From Facebook to Initial");
 		},
 		
 		handleListItemPress : function (evt) {
 			var context = evt.getSource().getBindingContext(),
-				entry = context.getModel().getProperty(context.getPath());
+				entry = this.getView().getModel().getProperty(context.getPath());
 			
 			this.getRouter().navTo("postDetail",{pId:entry.id} );
+		},
+		
+		printNames:function(data){
+			var i=0;
+			while(i<data.length){
+				var oItem = new sap.m.StandardListItem();
+				this.getView().byId("list").bindItems("items",oItem);
+				oItem.attachPress(this.handleListItemPress);
+				oItem.setType(sap.m.ListType.Active);
+				oItem.setTitle(data[i].name);
+				this.getView().byId("list").addItem(oItem);
+				i++;
+			}
+			
 		},
 		
 		handleSearch : function(evt){
@@ -44,6 +67,8 @@ sap.ui.controller("sap.ui.demo.myFiori.view.FacebookMaster", {
 				oModel.attachRequestCompleted(function() {
 					facebookController.getView().byId("list").setBusy(false);
 					console.log(oModel.getData());
+					facebookController.printNames(oModel.getData());
+					
 			    });
 				oModel.loadData(pathModel);		
 				this.getView().setModel(oModel);
